d9732737f8f36f4d5ee4dbdf0a0d63084f6c18b7
Taylor Keazirian <taylork26@gmail.com>
2021-03-14 10:47:09 -0500

    updated tests

--- test/folders-service.spec.js
+++ test/folders-service.spec.js
@@ -6,7 +6,7 @@ const FoldersService = require('../src/folders/folders-service');
 const { makeFoldersArray } = require('./folders.fixtures');
 const { makeNotesArray } = require('./notes.fixtures');
 
-describe.only('Folders endpoint', () => {
+describe('Folders endpoint', () => {
   let db;
   before('make knex instance', () => {
     db = knex({
@@ -32,17 +32,17 @@ describe.only('Folders endpoint', () => {
     })
 
     context('Given there are folders in the db', () => {
-      const testNotes = makeNotesArray();
       const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
 
       beforeEach('insert folders', () => {
         return db
-          .into('noteful')
-          .insert(testNotes)
+          .into('folders')
+          .insert(testFolders)
           .then(() => {
             return db
-              .into('noteful')
-              .insert(testFolders)
+              .into('notes')
+              .insert(testNotes)
           })
       })
 
@@ -54,6 +54,167 @@ describe.only('Folders endpoint', () => {
     })
   })
 
+  describe('POST /api/folders', () => {
+    it('creates a folder, responding with 201 and the new folder', () => {
+      const newFolder = {
+        folder_name: 'Test Folder Name'
+      }
 
+      return supertest(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .expect(201)
+        .expect(res=> {
+          expect(res.body.folder_name).to.eql(newFolder.folder_name)
+          expect(res.body).to.have.property('id')
+          expect(res.headers.location).to.eql(`/api/folders/${res.body.id}`)
+        })
+        .then(postRes => {
+          supertest(app)
+          .get(`/api/folders/${postRes.body.id}`)
+          .expect(postRes.body)
+        })
+    })
+
+    it(`responds with 400 and an error message when 'folder_name' is missing`, () => {
+      const newFolder = {
+        folder_name: ''
+      }
+      return supertest(app)
+        .post('/api/folders')
+        .send(newFolder)
+        .expect(400, {
+          error: {message: `Missing 'folder name' in request body`}
+        })
+    } )
+  })
+
+  describe('DELETE /api/folders/:folder_id', () => {
+    context('Given no folders', () => {
+      it('responds with 404', () => {
+        const folder_id = 123456;
+        return supertest(app)
+          .delete(`/api/folders/${folder_id}`)
+          .expect(404, {error: {message: `Folder doesn't exist`}})
+      })
+    })
+
+    context('Given there are folders in the db', () => {
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('responds with 204 and removes the folder', () => {
+        const idToRemove = 2;
+        const expectedFolders = testFolders.filter(folder => folder.id !== idToRemove)
+
+        return supertest(app)
+          .delete(`/api/folders/${idToRemove}`)
+          .expect(204)
+          .then(res => {
+            supertest(app)
+            .get('/api/folders')
+            .expect(expectedFolders)
+          })
+      })
+    })
+
+  })
+
+  describe('GET /api/folders/:folder_id', () => {
+
+    context('Given no folders', () => {
+      it('responds with 404', () => {
+        const folder_id = 123456;
+        return supertest(app)
+          .get(`/api/folders/${folder_id}`)
+          .expect(404, {error: {message: `Folder doesn't exist`}})
+      })
+    })
+
+    context('Given there are folders in the db', () => {
+
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('GET /api/folders/:folder_id responds with 200 and the specified folder', () => {
+        const folder_id = 2;
+        const expectedFolder = testFolders[folder_id - 1];
+        return supertest(app)
+          .get(`/api/folders/${folder_id}`)
+          .expect(200, expectedFolder)
+      })
+    })
+  })
+
+  describe('PATCH /api/folders/:folder_id', () => {
+    context('Given no folders', () => {
+      it('responds with 404', () => {
+        const folder_id = 123456;
+        return supertest(app)
+          .patch(`/api/folders/${folder_id}`)
+          .expect(404, {error: {message: `Folder doesn't exist`}})
+      })
+    })
+
+    context('Given there are folders in the db', () => {
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('responds with 204 and the updated folder', () => {
+        const idToUpdate = 2;
+        const updatedFolder = {
+          folder_name: 'Updated Folder Name'
+        }
+        const expectedFolder = {
+          ...testFolders[idToUpdate - 1],
+          ...updatedFolder
+        }
+        
+        return supertest(app)
+          .patch(`/api/folders/${idToUpdate}`)
+          .send(updatedFolder)
+          .expect(204)
+          .then(res => {
+            supertest(app)
+              .get(`/api/folders/${idToUpdate}`)
+              .expect(expectedFolder)
+          })
+      })
+
+    })
+  })
 
 })
--- test/notes-service.spec.js
+++ test/notes-service.spec.js
@@ -3,6 +3,10 @@ const supertest = require('supertest');
 const app = require('../src/app');
 const knex = require('knex');
 const NotesService = require('../src/notes/notes-service');
+const FoldersService = require('../src/folders/folders-service');
+const { makeNotesArray } = require('./notes.fixtures');
+const { makeFoldersArray } = require('./folders.fixtures');
+const e = require('express');
 
 describe('Notes endpoint', () => {
   let db;
@@ -30,15 +34,182 @@ describe('Notes endpoint', () => {
     })
 
     context('Given there are notes in the database', () => {
-      // beforeEach('insert notes', () => {
-      //   return db
-      //     .into('notes')
-          
-      // })
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('responds with 200 and all the notes', () => {
+        return supertest(app)
+          .get('/api/notes')
+          .expect(200, testNotes)
+      })
     })
   }) 
 
 
+  describe('POST /api/notes', () => {
+    it('creates a note, responding with 201 and the new note', () => {
+      const newNote = {
+        note_name: 'New Note',
+        content: 'New note content',
+        folder_id: 1
+      }
 
+      return supertest(app)
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.note_name).to.eql(newNote.note_name)
+          expect(res.body.content).to.eql(newNote.content)
+          expect(res.body.folder_id).to.eql(newNote.folder_id)
+          expect(res.body).to.have.property('id')
+          expect(res.headers.location).to.eql(`/api/notes/${res.body.id}`)
+        })
+        .then(postRes => {
+          supertest(app)
+          .get(`/api/notes/${postRes.body.id}`)
+          .expect(postRes.body)
+        })
+
+    })
+  })
+
+  describe('DELETE /api/notes/:note_id', () => {
+    context('Given no notes', () => {
+      it('responds with 404', () => {
+        const note_id = 123456;
+        return supertest(app)
+          .delete(`/api/notes/${note_id}`)
+          .expect(404, {error: {message: `Note doesn't exist`}})
+      })
+    })
+
+    context('Given there are notes in the db', () => {
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('responds with 204 and removes the note', () => {
+        const idToRemove = 2;
+        const expectedNotes = testNotes.filter(note => note.id !== idToRemove)
+
+        return supertest(app)
+          .delete(`/api/notes/${idToRemove}`)
+          .expect(204)
+          .then(res => {
+            supertest(app)
+            .get('/api/notes')
+            .expect(expectedNotes)
+          })
+      })
+
+    })
+  })
+
+  describe('GET /api/notes/:note_id', () => {
+    context('Given no notes', () => {
+      it('responds with 404', () => {
+        const note_id = 123456;
+        return supertest(app)
+          .get(`/api/notes/${note_id}`)
+          .expect(404, {error: {message: `Note doesn't exist`}})
+      })
+    })
+
+    context('Given there are notes in the db', () => {
+      const testFolders = makeFoldersArray();
+        const testNotes = makeNotesArray();
+  
+        beforeEach('insert folders', () => {
+          return db
+            .into('folders')
+            .insert(testFolders)
+            .then(() => {
+              return db
+                .into('notes')
+                .insert(testNotes)
+            })
+        })
+  
+      it('GET /api/notes/:note_id responds with 200 and the specified note', () => {
+        const note_id = 2;
+        const expectedNote = testNotes[note_id - 1];
+        return supertest(app)
+          .get(`/api/notes/${note_id}`)
+          .expect(200, expectedNote) 
+      })
+  
+    })
+  })
+
+  describe('PATCH /api/notes/:note_id', () => {
+    context('Given no notes', () => {
+      it('responds with 404', () => {
+        const note_id = 123456;
+        return supertest(app)
+          .patch(`/api/notes/${note_id}`)
+          .expect(404, {error: {message: `Note doesn't exist`}})
+      })
+    })
+
+    context('Given there are notes in the db', () => {
+      const testFolders = makeFoldersArray();
+      const testNotes = makeNotesArray();
+
+      beforeEach('insert folders', () => {
+        return db
+          .into('folders')
+          .insert(testFolders)
+          .then(() => {
+            return db
+              .into('notes')
+              .insert(testNotes)
+          })
+      })
+
+      it('responds with 204 and the updated folder', () => {
+        const idToUpdate = 1;
+        const updatedNote = {
+          note_name: 'Updated Note Name',
+          content: 'Updated note content',
+        }
+        const expectedNote = {
+          ...testNotes[idToUpdate - 1],
+          ...updatedNote
+        }
+
+        return supertest(app)
+          .patch(`/api/notes/${idToUpdate}`)
+          .send(updatedNote)
+          .expect(204)
+          .then(res => {
+            supertest(app)
+              .get(`/api/notes/${idToUpdate}`)
+              .expect(expectedNote)
+          })
+      })
+    })
+  })
 
 })
