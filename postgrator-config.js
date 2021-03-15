5ee8bfdb607c06083cc00d68bd747100f815c586
Taylor Keazirian <taylork26@gmail.com>
2021-03-15 13:53:55 -0500

    updated DB and TEST_DB

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,6 @@ module.exports = {
   "connectionString": 
     (process.env.NODE_ENV === 'test') 
       ? process.env.TEST_DB_URL 
-      : process.env.DB_URL
+      : process.env.DB_URL,
+  "ssl": { rejectUnauthorized: false }
 }
